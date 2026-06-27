d52a431df6b32d3de6c5fb26b575da4982f03a17
Rizwan Sayyad <164813687+SayyadRizwan@users.noreply.github.com>
2026-06-27 09:12:59 +0530

    Time: 3 ms (56.3%), Space: 45.7 MB (10.38%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,38 +1,29 @@
 class Solution {
-    void subsetSum(int[] nums,int idx,List<List<Integer>> ans, List<Integer> temp){
-        if(idx==nums.length){
-            List<Integer> copy = new ArrayList<>(temp);
-            ans.add(copy);
-            return ;
-        }
-        temp.add(nums[idx]);
-        subsetSum(nums,idx+1,ans,temp);
-        temp.remove(temp.size()-1);
-         subsetSum(nums,idx+1,ans,temp);
+    void subset(int[] nums, int idx , List<List<Integer>> ans , List<Integer> temp,HashSet<List<Integer>> hs){
+        if(idx == nums.length){
+            if(!hs.contains(new ArrayList<>(temp))){
+                ans.add(new ArrayList<>(temp));
+                hs.add(new ArrayList<>(temp));
+           }
+                return ; 
+             }
+            temp.add(nums[idx]);
+            subset(nums,idx+1,ans,temp,hs);
+            temp.remove(temp.size()-1);
+            subset(nums,idx+1,ans,temp,hs);
+
 
 
     }
-
+    
 
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+       HashSet<List<Integer>> hs = new HashSet<>();
         List<List<Integer>> ans = new ArrayList<>();
-         subsetSum(nums,0,ans,new ArrayList<Integer>());
-        List<List<Integer>> subset = new ArrayList<>();
-        HashSet<List<Integer>> hs = new HashSet<>();
-        for(List<Integer> elem : ans){
-            
-            Collections.sort(elem);
-            if(!hs.contains(elem)){
-                subset.add(elem);
-            }
-            hs.add(elem);
-        }
-
-
-
-
-         return subset;
+        subset(nums,0,ans,new ArrayList<Integer>(),hs);
+        return ans;
         
     }
 }
