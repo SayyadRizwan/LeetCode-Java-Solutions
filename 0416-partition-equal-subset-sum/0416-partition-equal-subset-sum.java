c2841f5502808a00bead526db0c0e71c44fb2e7b
Rizwan Sayyad <164813687+SayyadRizwan@users.noreply.github.com>
2026-08-03 09:18:32 +0530

    Time: 102 ms (13.52%), Space: 129.6 MB (5.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,26 +1,23 @@
 class Solution {
-
-    boolean subset(int[] nums,int sum,int idx,int TotalSum, Boolean dp[][]){
-        if(idx==nums.length)return false;
-        if(sum>TotalSum)return false;
+    boolean partitionEligible(int[] nums,int idx,int sum){
+        if(idx>=nums.length)return false;
+        if(sum==0)return true;
+        if(sum<0)return false;
         if(dp[idx][sum]!=null)return dp[idx][sum];
-        if(TotalSum- sum == sum) return true;
+        boolean take = partitionEligible(nums,idx+1,sum-nums[idx]);
+        boolean skip = partitionEligible(nums,idx+1,sum);
+        return dp[idx][sum] = take || skip;
 
-        boolean take = subset(nums,sum+nums[idx],idx+1,TotalSum,dp);
-        boolean skip = subset(nums,sum,idx+1,TotalSum,dp);
-        return dp[idx][sum] =  take||skip;
-        
     }
-
-
+    Boolean[][] dp ;
     public boolean canPartition(int[] nums) {
         int sum=0;
-       
-        for(int num : nums){
-            sum+=num;
-        }
-        if(sum%2==1)return false;
-         Boolean dp[][] = new Boolean[nums.length][sum+1];
-      return  subset(nums,0,0,sum,dp);
+          for(int num: nums)sum+=num;
+        dp = new Boolean[nums.length+1][sum];
+        
+      
+        if(sum%2!=0)return false;
+        return partitionEligible(nums,0,sum/2);
+        
     }
 }
