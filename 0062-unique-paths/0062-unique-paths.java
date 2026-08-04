49557b0c41ed3809b7b253b0600482d9dcd847a0
Rizwan Sayyad <164813687+SayyadRizwan@users.noreply.github.com>
2026-08-04 08:59:09 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,17 @@
 class Solution {
-int path(int m, int n,int dp[][]){
-    if(m==1 && n==1)return 1;
-    if(m==0 || n==0)return 0;
-    if(dp[m][n]!=-1)return dp[m][n];
-    int right= path(m,n-1,dp);
-    int left = path(m-1,n,dp);
-    dp[m][n]=right+left;
-    return dp[m][n];
-
-}
+    int totalPath(int m,int n){
+        if(m<0 || n<0)return 0;
+        if(m==0 && n==0)return 1;
+        if (dp[m][n]!=-1)return dp[m][n];
+        int down = totalPath(m-1,n);
+        int right = totalPath(m,n-1);
+        return dp[m][n]=down+ right;
+    }
+        int[][] dp;
 
     public int uniquePaths(int m, int n) {
-        int[][] dp= new int[m+1][n+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return path(m,n,dp);
+        dp = new int[m+1][n+1];
+        for(int[] arr: dp)Arrays.fill(arr,-1);
+        return totalPath(m-1,n-1);
     }
 }
