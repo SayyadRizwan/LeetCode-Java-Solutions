15dd338f81b38b7f77e11acf5ab328743209146b
Rizwan Sayyad <164813687+SayyadRizwan@users.noreply.github.com>
2026-09-01 20:20:56 +0530

    Time: 133 ms (13.84%), Space: 46.8 MB (87.48%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,22 +1,14 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         int count=0;
-        HashMap<Integer,List<List<Integer>>> hm = new HashMap<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
-                List<Integer> temp = new ArrayList<>();
-                temp.add(i);
-                temp.add(j);
+               
                 int val = nums1[i]+nums2[j];
                  List<List<Integer>> l = new ArrayList<>();
-                l.add(temp);
-                if(hm.containsKey(val)){
-                    hm.get(val).add(temp);
-                }
-               
-                else{
-                    hm.put(val,l);
-                }
+                
+                hm.put(val,hm.getOrDefault(val,0)+1);
             }
         }
         for(int i=0;i<nums3.length;i++){
@@ -24,7 +16,7 @@ class Solution {
                 int val =nums3[i]+nums4[j];
                 int rem = 0-val;
                 if(hm.containsKey(rem)){
-                    count+=hm.get(rem).size();
+                    count+=hm.get(rem);
                 }
             }
         }
